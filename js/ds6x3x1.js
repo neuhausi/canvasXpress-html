98783d6aad8a614cde9a517e84c6502c5b5e0826
Isaac Neuhaus <imnphd@gmail.com>
2024-09-15 01:51:39 -0400

    refactor

---
+++ js/ds6x3x1.js
@@ -0,0 +1,9 @@
+var ds6x3x1 = [
+  ["Month", "Series A", "Series B", "Series C", "Quarter"],
+  ["Jan", 10, 5, 2, "Q1"],
+  ["Feb", 11, 8, 3, "Q1"],
+  ["Mar", 13, 10, 6, "Q1"],
+  ["Apr", 4, 12, 4, "Q2"],
+  ["May", 18, 15, 8, "Q2"],
+  ["Jun", 21, 10, 6, "Q2"]
+]
